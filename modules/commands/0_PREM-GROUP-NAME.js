8439fc2612b21fb788ee573b442a4c57f184b701
prem-project3608 <144128294+prem-project3608@users.noreply.github.com>
2024-04-13 00:13:01 +0530

    Delete modules/commands directory

--- modules/commands/0_PREM-GROUP-NAME.js
+++
@@ -1,17 +0,0 @@
-module.exports.config = {
-	name: "groupname",
-	version: "1.0.0", 
-	hasPermssion: 0,
-	credits: "PREM BABU",
-	description: "THIS BOT WAS MADE BY MR PREM BABU",
-	commandCategory: "CHANGE GROUP NAME", 
-	usages: "PREFIX", 
-	cooldowns: 0,
-	dependencies: [] 
-};
-
-module.exports.run = async function({ api, event, args }) {
-	var name = args.join(" ")
-	if (!name) api.sendMessage("बॉस साथ में ग्रुप का नाम लिखो जो आप रखना चाहते हो 😐✌️", event.threadID, event.messageID)
-	else api.setTitle(name, event.threadID, () => api.sendMessage(`अब इस ग्रुप का नाम ये है 👉 ${name}\n━━━━━━━━━━━━━━━━━━━━━━━\nOWNER  𒁍 MR PREM BABU 🌺`, event.threadID, event.messageID));
-}
