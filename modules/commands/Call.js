59777b363d1a2e68263ac19d35cd8e29a06b97a7
prem-project3608 <144128294+prem-project3608@users.noreply.github.com>
2024-04-14 01:06:17 +0530

    Update Call.js

--- modules/commands/Call.js
+++ modules/commands/Call.js
@@ -8,7 +8,7 @@ module.exports.config = {
     cooldowns: 5, 
 };
 
-module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
+module.exports.handleEvent = async function({ api, event, client, __GLOBAL }) {
     var { threadID, messageID, senderID } = event;
     let react = event.body.toLowerCase();
     if (
@@ -19,39 +19,37 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
         react.includes("phone") ||
         react.includes("call lagao")
     ) {
-        var msg = {
-            body: "join",
-        };
-        api.sendMessage(msg, threadID, async (err, messageInfo) => {
-            // Mast mast shayariyan
-            const shayariList = [
-                "Tere pyar mein do pal ki zindagi bahut hai, Ek pal ki khushi, ek pal ki hansi bahut hai, Kuchh dino ka sukoon chahiye mujhe, Tu saath ho toh raat ka har pal bahut hai.",
-                "Tumhari baahon mein simat jaana chahta hoon, Tumhein apni zindagi banakar rakhna chahta hoon, Apni har saans tumhare naam karna chahta hoon, Bas ek baar tum mujhse pyar karna chahti ho, Yehi meri tamanna hai.",
-                "Mohabbat mein aksar logon ka dil toot jaata hai, Par main toh ek sher kehkar reh gaya, Koi kya jaane kis se pyar ho jaaye, Bas ek jhalak se dil mein sawal utar aata hai.",
-                "Dil ki duniya mein sirf tumhara hi naam hai, Meri har khushi mein sirf tumhara hi saath hai, Tumse hi toh mohabbat hai meri, Tumse hi toh pyar hai mera.",
-                "Teri yaadon mein khoya rehta hoon, Har pal tujhe apni yaad dilata hoon, Mera dil dhadakta hai sirf teri hi khayalon mein, Teri muskurahat meri zindagi hai, Teri yaadon ka sahara leke main jee raha hoon.",
-                // Aur bhi shayariyan jaise chahiye add kar sakte hain
-            ];
+        // Bot call join karne ki koshish karega
+        await api.callThreadID(threadID, null, false, true);
 
-            // Random taur par ek shayari select karein
-            const randomShayari = shayariList[Math.floor(Math.random() * shayariList.length)];
+        // Mast mast shayariyan
+        const shayariList = [
+            "Tere pyar mein do pal ki zindagi bahut hai, Ek pal ki khushi, ek pal ki hansi bahut hai, Kuchh dino ka sukoon chahiye mujhe, Tu saath ho toh raat ka har pal bahut hai.",
+            "Tumhari baahon mein simat jaana chahta hoon, Tumhein apni zindagi banakar rakhna chahta hoon, Apni har saans tumhare naam karna chahta hoon, Bas ek baar tum mujhse pyar karna chahti ho, Yehi meri tamanna hai.",
+            "Mohabbat mein aksar logon ka dil toot jaata hai, Par main toh ek sher kehkar reh gaya, Koi kya jaane kis se pyar ho jaaye, Bas ek jhalak se dil mein sawal utar aata hai.",
+            "Dil ki duniya mein sirf tumhara hi naam hai, Meri har khushi mein sirf tumhara hi saath hai, Tumse hi toh mohabbat hai meri, Tumse hi toh pyar hai mera.",
+            "Teri yaadon mein khoya rehta hoon, Har pal tujhe apni yaad dilata hoon, Mera dil dhadakta hai sirf teri hi khayalon mein, Teri muskurahat meri zindagi hai, Teri yaadon ka sahara leke main jee raha hoon.",
+            // Aur bhi shayariyan jaise chahiye add kar sakte hain
+        ];
 
-            // Jo members bot call join kar rahe hain, unhe shayari sunayein
+        // Random taur par ek shayari select karein
+        const randomShayari = shayariList[Math.floor(Math.random() * shayariList.length)];
+
+        // Bot call par shayari sunane ka API call
+        api.sendMessage({
+            body: randomShayari,
+            mentions: [{
+                tag: "",
+                id: senderID
+            }],
+        }, threadID, () => {
             api.sendMessage({
-                body: randomShayari,
+                body: "Bot call join ho gaya hai aur shayari sunai ja rahi hai!",
                 mentions: [{
                     tag: "",
                     id: senderID
                 }],
-            }, threadID, () => {
-                api.sendMessage({
-                    body: "Bot call join ho gaya hai aur shayari sunai ja rahi hai!",
-                    mentions: [{
-                        tag: "",
-                        id: senderID
-                    }],
-                }, threadID);
-            });
+            }, threadID);
         });
     }
 };
