89a7cb9e40ed014bd35bf3a56c6f2384eec334d1
GITHUB78600 <githup3@yandex.com>
2024-08-09 15:49:33 +0530

    Update and rename 0_PREM-AUTO-SEEN.js to 0_DEEPU-AUTO-SEEN.js

--- modules/commands/0_DEEPU-AUTO-SEEN.js
+++ modules/commands/0_DEEPU-AUTO-SEEN.js
@@ -25,10 +25,10 @@ module.exports. run = async ({ api, event, args }) => {
    try {
      if (args[0] == 'on') {
        fs.writeFileSync(pathFile, 'true');
-       api.sendMessage('सभी मैसेज ऑटोसिन ऑन हो गया प्रेम बॉस 🤐👈', event.threadID, event.messageID);
+       api.sendMessage('सभी मैसेज ऑटोसिन ऑन हो गया दीपू बॉस 🤐👈', event.threadID, event.messageID);
      } else if (args[0] == 'off') {
        fs.writeFileSync(pathFile, 'false');
-       api.sendMessage('जो वर्ड या इमोजी मेरे कमांड में नही है। उसका ऑटोसीन ऑफ कर दिया प्रेम बॉस 🤐🤐', event.threadID, event.messageID);
+       api.sendMessage('जो वर्ड या इमोजी मेरे कमांड में नही है। उसका ऑटोसीन ऑफ कर दिया दीपू बॉस 🤐🤐', event.threadID, event.messageID);
      } else {
        api.sendMessage('सॉरी बॉस कुछ गड़बड़ है। 🤔👈', event.threadID, event.messageID);
      }
