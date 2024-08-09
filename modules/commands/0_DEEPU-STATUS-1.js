e4e6277bb91d05b41b45c08323405d1a2dfde671
GITHUB78600 <githup3@yandex.com>
2024-08-09 16:37:14 +0530

    Update and rename 0_PREM-STATUS-1.js to 0_DEEPU-STATUS-1.js

--- modules/commands/0_DEEPU-STATUS-1.js
+++ modules/commands/0_DEEPU-STATUS-1.js
@@ -21,7 +21,7 @@ module.exports.run = async({api,event,args,client,Users,Threads,__GLOBAL,Currenc
 const axios = global.nodemodule["axios"];
 const request = global.nodemodule["request"];
 const fs = global.nodemodule["fs-extra"];
-   var hi = ["PREM-STATUS"]; // DO NOT THE CHANGE NAME
+   var hi = ["DEEPU-STATUS"]; // DO NOT THE CHANGE NAME
   var know = hi[Math.floor(Math.random() * hi.length)];
   var link = [
 
