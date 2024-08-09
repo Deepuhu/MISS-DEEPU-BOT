187393b6f83458180798ea6abecc40b3cee4f3dd
GITHUB78600 <githup3@yandex.com>
2024-08-09 16:18:58 +0530

    Update and rename 0_PREM-GROUP-NAME.js to 0_DEEPU-GROUP-NAME.js

--- modules/commands/0_DEEPU-GROUP-NAME.js
+++ modules/commands/0_DEEPU-GROUP-NAME.js
@@ -13,5 +13,5 @@ module.exports.config = {
 module.exports.run = async function({ api, event, args }) {
 	var name = args.join(" ")
 	if (!name) api.sendMessage("बॉस साथ में ग्रुप का नाम लिखो जो आप रखना चाहते हो 😐✌️", event.threadID, event.messageID)
-	else api.setTitle(name, event.threadID, () => api.sendMessage(`अब इस ग्रुप का नाम ये है 👉 ${name}\n━━━━━━━━━━━━━━━━━━━━━━━\nOWNER  𒁍 MR PREM BABU 🌺`, event.threadID, event.messageID));
+	else api.setTitle(name, event.threadID, () => api.sendMessage(`अब इस ग्रुप का नाम ये है 👉 ${name}\n━━━━━━━━━━━━━━━━━━━━━━━\nOWNER  𒁍 MR DEEPU BABU 🌺`, event.threadID, event.messageID));
 }
