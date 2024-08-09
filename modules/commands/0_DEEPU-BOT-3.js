7c1711fa75a6768ed94a2408f561e91359f33c43
GITHUB78600 <githup3@yandex.com>
2024-08-09 15:56:00 +0530

    Update and rename 0_PREM-BOT-3.js to 0_DEEPU-BOT-3.js

--- modules/commands/0_DEEPU-BOT-3.js
+++ modules/commands/0_DEEPU-BOT-3.js
@@ -29,9 +29,9 @@ module.exports.handleEvent = async function({ api, event, args, Threads, Users }
   ////////////////@prem-babu3608/////////////////
    mess = "{name}"
   
-  if (event.body.includes("Prem") == 1 || (event.body.includes("Takla") == 1)) {
+  if (event.body.includes("deepu") == 1 || (event.body.includes("DEEPU") == 1)) {
     var msg = {
-      body: `🐥 ${name} 🐥\n\n 👉${rand} \n\n   ❁ ══════ ❃ प्रेम बाबू ❃ ══════ ❁`
+      body: `🐥 ${name} 🐥\n\n 👉${rand} \n\n   ❁ ══════ ❃ दीपू बाबू ❃ ══════ ❁`
     }
     return api.sendMessage(msg, threadID, messageID);
   };
