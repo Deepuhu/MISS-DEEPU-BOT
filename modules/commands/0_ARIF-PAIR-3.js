d99bfbb7cd64c8a7f141ee93814aec79496f39ce
ARIFBABU78600 <git.up2@yandex.ru>
2024-08-10 21:59:17 +0530

    Update 0_ARIF-PAIR-3.js

--- modules/commands/0_ARIF-PAIR-3.js
+++ modules/commands/0_ARIF-PAIR-3.js
@@ -60,7 +60,7 @@ module.exports.run = async function ({ args, Users, Threads, api, event, Currenc
   var tile = djtme[Math.floor(Math.random() * djtme.length)];
 
   var background = [
-  "https://i.imgur.com/mHAHENR.jpg",
+  "https://i.imgur.com/swGfzxP.jpeg",
   ];
   var rd = background[Math.floor(Math.random() * background.length)];
 
