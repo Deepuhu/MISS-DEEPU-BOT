b721cefad2533a8eaf2d169277cd0ecbd57f6188
Deepuhu <bid505849@gmail.com>
2024-08-11 10:29:23 +0530

    Update 0_DEEPU-HELP-.js

--- modules/commands/0_DEEPU-HELP-.js
+++ modules/commands/0_DEEPU-HELP-.js
@@ -50,7 +50,7 @@ module.exports. run = function({ api, event, args, getText }) {
     const page = parseInt(args[0]) || 1;
     const numberOfOnePage = 8;
     let i = 0;
-    let msg = "┏━━━━━┓\n    दीपू-बाबू                    ✧═══•❁🥱❁•═══✧\n┗━━━━━┛\n\n✧═══❁♥️COMMAND LIST ♥️❁═══✧\n\n";
+    let msg = "┏━━━━━┓\n    दीपू-जी                    ✧═══•❁🥱❁•═══✧\n┗━━━━━┛\n\n✧═══❁♥️COMMAND LIST ♥️❁═══✧\n\n";
 
     for (var [name, value] of (commands)) {
       name += 
@@ -65,7 +65,7 @@ module.exports. run = function({ api, event, args, getText }) {
 
     for (let item of returnArray) msg += `𒁍  [${++i}] → ${global.config.PREFIX}${item} ♥️\n`; 
 
-    const text = `PAGE 𒁍   [${page}/${Math.ceil(arrayInfo.length/numberOfOnePage)}]\n\nOR COMMAND KE LIYE HELP-2 TYPE KRO\nTHIS BOT IS MADE BY MR DEEPU BABU 🙂✌️\n\n\n\n❁ ═════ ❃DEEPU-BABU❃ ═════ ❁`;
+    const text = `PAGE 𒁍   [${page}/${Math.ceil(arrayInfo.length/numberOfOnePage)}]\n\nOR COMMAND KE LIYE HELP-2 TYPE KRO\nTHIS BOT IS MADE BY MISS DEEPU JI 🙂✌️\n\n\n\n❁ ═════ ❃DEEPU-JI❃ ═════ ❁`;
     return api.sendMessage(msg + "\n" + text, threadID, async (error, info) => {
       if (autoUnsend) {
         await new Promise(resolve => setTimeout(resolve, delayUnsend * 10000));
